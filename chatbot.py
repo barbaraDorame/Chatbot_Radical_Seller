4622bc4b742750cac707252e6e791234e3fbf445
Barbaragd <galindodorame@gmail.com>
2018-04-24 16:51:02 -0700

    Init commit

---
+++ chatbot.py
@@ -0,0 +1,21 @@
+import spacy
+nlp = spacy.load("es_core_news_md")
+
+class ChatBot:
+    def __init__(self, texto):
+        self.text = texto
+
+
+    def limpieza(self, texto):
+        texto = texto.lower()
+        nlp(texto)
+        
+
+
+    def responder(self, texto):
+
+    def clasificar(self, texto):
+
+    def analisis_sentimientos(self, texto):
+
+    def procesar():
