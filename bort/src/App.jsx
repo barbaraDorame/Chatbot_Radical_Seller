17e48bf7adb0bc1038c98a7512facea7926634c4
Rafael Castillo <rafael@trfs.me>
2018-04-30 15:42:11 -0700

    Agregar logica de conversacion

--- bort/src/App.jsx
+++ bort/src/App.jsx
@@ -3,12 +3,16 @@ import moment from 'moment';
 import Chat from './components/Chat';
 import './App.css';
 
-let counter = 0;
-
 moment.locale('es');
 
-const crearMensaje = (texto, hora, humano = true) => ({
-  texto, hora, humano, id: (counter += 1),
+const crearMensaje = (id, texto, hora, humano = true) => ({
+  texto, hora, humano, id,
+});
+
+const convertirMensaje = ({
+  id, texto, hora_creacion, humano,
+}) => ({
+  id, texto, hora: moment(hora_creacion), humano,
 });
 
 class App extends Component {
@@ -17,33 +21,54 @@ class App extends Component {
     this.handleAddMessage = this.handleAddMessage.bind(this);
     this.handleCambiarMensaje = this.handleCambiarMensaje.bind(this);
     this.state = {
-      mensajes: [
-        crearMensaje('Hola', moment(), false),
-        crearMensaje('Hola', moment(), true),
-        crearMensaje('¿Como estas?', moment(), false),
-        crearMensaje('Bien, ¿y tú?', moment(), true),
-      ],
+      loaded: true,
+      conversationId: null,
+      mensajes: [],
       mensajeActual: '',
     };
   }
 
+  componentDidMount() {
+    fetch('/api/conversacion', { method: 'post' })
+      .then(res => res.json())
+      .then((conversation) => {
+        this.setState(prevState => ({
+          ...prevState,
+          loaded: true,
+          conversationId: conversation.id,
+          mensajes: conversation.mensajes.map(convertirMensaje),
+        }));
+      });
+  }
+
   handleAddMessage(event) {
     event.preventDefault();
-    this.setState(prevState => ({
-      ...prevState,
-      mensajes: [
-        ...prevState.mensajes,
-        crearMensaje(prevState.mensajeActual, moment()),
-      ],
-      mensajeActual: '',
-    }));
+    const texto = this.state.mensajeActual;
+    fetch(`/api/conversacion/${this.state.conversationId}/mensajes`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'post',
+      body: JSON.stringify({ texto }),
+    }).then(res => res.json())
+      .then((msg) => {
+        this.setState(prevState => ({
+          ...prevState,
+          mensajes: [
+            ...prevState.mensajes,
+            convertirMensaje(msg),
+          ],
+        }));
+      });
   }
 
   handleCambiarMensaje(event) {
-    const text = event.target.value;
+    const texto = event.target.value;
+
     this.setState(prevState => ({
       ...prevState,
-      mensajeActual: text,
+      mensajeActual: texto,
     }));
   }
 
@@ -54,8 +79,14 @@ class App extends Component {
       state: {
         mensajes,
         mensajeActual,
-      } 
-    }= this;
+        loaded,
+      },
+    } = this;
+
+    if (!loaded) {
+      return <div>Cargando...</div>;
+    }
+
     return (
       <Chat
         mensajes={mensajes}
