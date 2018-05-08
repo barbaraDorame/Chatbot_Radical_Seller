7ca7133a8484fdfa698710a4affe360439d34e02
Rafael Castillo <rafael@trfs.me>
2018-05-08 13:18:37 -0700

    Agregar impresion al entrenamiento

--- bort.py
+++ bort.py
@@ -5,20 +5,24 @@ from clasificador_intencion import ClasificadorIntencion
 
 @click.group()
 def bort():
-    click.echo('Hello twitch chat')
+    pass
 
 
 @bort.command()
 @click.argument('input', nargs=-1, type=click.File('r'))
 @click.argument('out', nargs=1, type=click.Path())
-@click.option('--init-model', default=None, type=click.File())
 @click.option('--nlp-model', default='es_core_news_sm', type=click.STRING)
-def entrenar(input, out, init_model, nlp_model):
+def entrenar(input, out, nlp_model):
     input_sets = [json.load(input_set) for input_set in input]
     examples = [example for input_set in input_sets for example in input_set]
 
     model = ClasificadorIntencion(nlp_model)
     model.entrenar(examples)
+
+    click.echo('Entrenando con {} ejemplos'.format(len(examples)))
+    click.echo('Intenciones entrenadas:')
+    for _, v in model.intent_idx.items():
+        click.echo(' - ' + v)
 
     model.persistir(out)
 
@@ -30,7 +34,7 @@ def predecir(model, text):
     model = ClasificadorIntencion.cargar(model)
 
     pred, prob = model.predecir(text)
-    prob.sort(reverse=True)
+    prob.sort()
     click.echo(pred)
     click.echo(prob)
 
